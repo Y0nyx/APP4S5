d55546a0ea63fbd7a3ec386a603fff5628879050
Y0nyx <veic1101@usherbrooke.ca>
2022-10-15 12:26:35 -0400

    Rotation

--- PictureManager.py
+++ PictureManager.py
@@ -8,12 +8,24 @@ def revertAbberation(img, num, den):
     zplane.zplane(den, num)
 
     # Pour comparer, enregistrer l'image avec l'abberation
-    mpimg.imsave("img\\gold_abberations.png", arr=img)
+    mpimg.imsave("img\\goldhill_abberations.png", arr=img)
 
     # Filtre et enregistre l'image finale
     FiltedImg = signal.lfilter(den, num, img)
-    mpimg.imsave("imgFinale\\gold_abberations_Finale.png", arr=FiltedImg)
+    mpimg.imsave("imgFinale\\goldhill_abberations_Finale.png", arr=FiltedImg)
 
 def rotation90degree (img):
-    #lecture nécessaire
+    # Trouver les longueur des column and row
+    row = len(img)
+    column = len(img[0])
+    EmptyArray = np.zeros((column, row))
+    # Effectuer le changement de coordonne grace a une double for loop
+    for i in range(0, row):
+        for j in range(0, column):
+            coordX = -row + 1 + i
+            coordY = column - 1 - j
+            # Multiplie
+            x, y = np.matmul([[0, 1], [-1, 0]], [coordX, coordY])
+            EmptyArray[x][y] = img[coordX][coordY]
+    mpimg.imsave("imgFinale\\goldhill_rotate_Finale.png", arr=EmptyArray)
 
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import numpy as np
+import matplotlib.image as mpimg
 import PictureManager
 
 if __name__ == '__main__':
+    # abberation
     num = np.poly([0.9*np.exp(np.pi/2j), 0.9*np.exp(-np.pi/2j), 0.95*np.exp(np.pi/8j), 0.95*np.exp(-np.pi/8j)])
     den = np.poly([-0.99, -0.99, 0.8])
     img = np.load("img\\goldhill_aberrations.npy")
-    img = PictureManager.revertAbberation(img, num, den)
+    PictureManager.revertAbberation(img, num, den)
+
+    #rotation
+    img_color = mpimg.imread("img\\goldhill_rotate.png")
+    img = np.mean(img_color, -1)
+    PictureManager.rotation90degree(img)
 
